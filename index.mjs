99336a622c7e8c8310261a9ffbb1758b74d286cd
Darlan Alves <745095+darlanalves@users.noreply.github.com>
2024-08-18 00:17:50 +0200

    chore: update repo

--- index.mjs
+++ index.mjs
@@ -1,11 +1,8 @@
 import createServer from "@cloud-cli/http";
-import { mkdir, readFile, rm, writeFile } from "fs/promises";
-import { spawnSync } from "child_process";
-import { createHash } from "crypto";
-import { join, resolve, basename } from "path";
+import { join, resolve } from "path";
 import { createReadStream, existsSync, statSync } from "fs";
 
-const baseDomain = process.env.BASE_DOMAIN;
+const nameRe = /@[a-z]+\/[a-z]+/;
 const workingDir = process.env.DATA_PATH;
 const mimeTypes = {
   css: "text/css",
@@ -14,13 +11,36 @@ const mimeTypes = {
   mjs: "text/javascript",
 };
 
-createServer(async function (request, response) {
+createServer(function (request, response) {
   try {
     if (["OPTIONS", "GET"].includes(request.method) === false) {
       return notFound(response);
     }
 
-    onFetch(request, response);
+    const url = new URL(request.url, "http://localhost");
+    const pathname = url.pathname.slice(1);
+
+    if (nameRe.test(pathname)) {
+      return notFound(response);
+    }
+
+    const path = resolve(url.pathname);
+    const file = join(workingDir, path);
+
+    if (!existsSync(file) || !statSync(file).isFile()) {
+      return notFound(response);
+    }
+
+    const extension = path.split(".").pop();
+    if (!url.searchParams.has("nocache")) {
+      response.setHeader("Cache-Control", "max-age=86400");
+    }
+
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS, POST");
+    response.setHeader("Content-Type", mimeTypes[extension] || "text/plain");
+
+    createReadStream(file).pipe(response);
   } catch (e) {
     console.log(e);
     if (!response.headersSent) {
@@ -28,146 +48,6 @@ createServer(async function (request, response) {
     }
   }
 });
-
-const nameRe = /@[a-z]+\/[a-z]+/;
-async function onFetch(request, response) {
-  const url = new URL(request.url, "http://localhost");
-  const pathname = url.pathname.slice(1);
-
-  // @foo/bar
-  const parts = pathname.split("/");
-
-  if (nameRe.test(pathname)) {
-    return notFound(response);
-  }
-
-  const path = resolve(url.pathname);
-  const file = join(workingDir, path);
-
-  if (!existsSync(file) || !statSync(file).isFile()) {
-    return notFound(response);
-  }
-
-  const extension = path.split(".").pop();
-  if (!url.searchParams.has("nocache")) {
-    response.setHeader("Cache-Control", "max-age=86400");
-  }
-
-  response.setHeader("Access-Control-Allow-Origin", "*");
-  response.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS, POST");
-  response.setHeader("Content-Type", mimeTypes[extension] || "text/plain");
-
-  createReadStream(file).pipe(response);
-}
-
-async function onBackup(request, response) {
-  if (request.headers.authorization !== authKey) {
-    console.log("unauthorized key", request.headers.authorization);
-    badRequest(response, "Unauthorized");
-    return;
-  }
-
-  const url = new URL(request.url, "http://localhost");
-  let name = basename(resolve(url.pathname.slice(1)));
-
-  const aliasFile = join(workingDir, name + ".alias");
-  if (existsSync(aliasFile) && statSync(aliasFile).isFile()) {
-    name = await readFile(aliasFile, "utf8");
-  }
-
-  const dir = join(workingDir, name);
-  if (!existsSync(dir) || !statSync(dir).isDirectory()) {
-    notFound(response);
-    return;
-  }
-
-  const sh = spawnSync("tar", ["c", "-z", "-C", dir, "-f", "-", "."]);
-  if (sh.status) {
-    const [_, stdout, stderr] = sh.output;
-    console.log(stdout.toString("utf-8"));
-    console.log(stderr.toString("utf-8"));
-
-    badRequest(response, "Failed to generate file");
-    return;
-  }
-
-  response.setHeader("Content-Type", "application/x-gzip");
-  response.setHeader(
-    "Content-Disposition",
-    `attachment; filename="${name}.tgz"`
-  );
-
-  response.end(sh.stdout);
-}
-
-async function onDeploy(request, response) {
-  if (request.headers.authorization !== authKey) {
-    console.log("unauthorized key", request.headers.authorization);
-    badRequest(response);
-    return;
-  }
-
-  let file;
-
-  try {
-    const buffer = Buffer.concat(await request.toArray());
-    const hash = createHash("sha256").update(buffer).digest("hex").slice(0, 8);
-
-    const dir = join(workingDir, hash);
-    file = join(workingDir, hash + ".tgz");
-
-    await writeFile(file, buffer);
-    await mkdir(dir, { recursive: true });
-
-    const sh = spawnSync("tar", [
-      "-xzf",
-      file,
-      "--overwrite",
-      "--directory",
-      dir,
-    ]);
-
-    if (sh.status) {
-      throw new Error("Failed to extract files: " + sh.error);
-    }
-
-    const manifest = join(dir, "package.json");
-    let alias = "";
-    let previousDir = "";
-
-    if (existsSync(manifest)) {
-      const json = JSON.parse(await readFile(manifest));
-
-      if (json.name) {
-        const aliasFile = join(workingDir, json.name + ".alias");
-        previousDir =
-          (existsSync(aliasFile) && (await readFile(aliasFile, "utf-8"))) || "";
-        await writeFile(aliasFile, hash, "utf-8");
-        alias = json.name;
-      }
-    }
-
-    if (previousDir) {
-      await rm(join(workingDir, previousDir), { recursive: true, force: true });
-    }
-
-    response.writeHead(201).end(
-      JSON.stringify({
-        status: "success",
-        url: `https://${hash}.${baseDomain}`,
-        alias: alias ? `https://${alias}.${baseDomain}` : undefined,
-      })
-    );
-  } catch (error) {
-    response
-      .writeHead(400)
-      .end(`{"status": "error", "error": ${JSON.stringify(String(error))}}`);
-  } finally {
-    if (file && existsSync(file)) {
-      await rm(file);
-    }
-  }
-}
 
 function notFound(response) {
   response.writeHead(404).end("Not found");
